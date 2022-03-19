69d5531534b560d21d0ed701eba0b540fa3b5d5e
Amanda Medeiros <medeiros.amandas@gmail.com>
2022-03-19 00:02:32 -0300

    adicionando página e funções para detalhar cada um dos detalhes

--- src/main/java/com/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import com.eventosapp.models.Evento;
 import com.eventosapp.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +30,14 @@ public class EventoController {
         ModelAndView mv = new ModelAndView("index");
         Iterable<Evento> eventos = er.findAll();
         mv.addObject("eventos", eventos);
+        return mv;
+    }
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalhesEvento(@PathVariable("codigo") Long codigo){
+
+        Evento evento = er.findByCodigo(codigo);
+        ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+        mv.addObject("evento", evento);
         return mv;
     }
 }
--- src/main/java/com/eventosapp/repository/EventoRepository.java
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -4,4 +4,5 @@ import com.eventosapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
+    Evento findByCodigo(Long codigo);
 }
